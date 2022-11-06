b716b6a967780dfd1cd4777c51bb45e8b5d754b4
ahamedazmie5 <ahamedazmie5@gmail.com>
2022-11-06 15:22:32 +0530

    FE 1 Commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import AdminHome from "./screens/AdminHome";
+import AdminManagement from "./screens/AdminManagement";
+import StudentManagement from "./screens/StudentManagement";
+import AddStudents from "./screens/AddStudents";
+import ViewStudents from "./screens/ViewStudents";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Signup">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="AdminHome" component={AdminHome} />
+        <Stack.Screen name="AdminManagement" component={AdminManagement} />
+        <Stack.Screen name="StudentManagement" component={StudentManagement} />
+        <Stack.Screen name="AddStudents" component={AddStudents} />
+        <Stack.Screen name="ViewStudents" component={ViewStudents} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Login.jsx
@@ -0,0 +1,101 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Image, Button } from "react-native";
+import { TouchableOpacity } from "react-native";
+import axios from "axios";
+import AdminHome from "./AdminManagement";
+
+export default function Login({navigation}) {
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const login = async (e) => {
+    e.preventDefault();
+    const URL = `http://192.168.194.251:8000/user/signin`;
+    axios
+      .post(URL, { email: email, password: pwd })
+      .then((res) => {
+        alert(res.data.userRole);
+        navigation.navigate("Home", {
+          userID: res.data.userId,
+          userRole: res.data.userRole,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Welcome Back</Text>
+      <Image
+        style={{ width: 250, height: 250 }}
+        source={require("../assets/logo.png")}
+      />
+      <TextInput
+        placeholder="Email"
+        style={styles.textInput}
+        onChangeText={(e) => setEmail(e)}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        onChangeText={(e) => setPwd(e)}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={AdminHome}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.subtitle}>Don't have an account ?</Text>
+      <TouchableOpacity
+        style={{ color: "#5D3FD3" }}
+        onPress={() => navigation.navigate("Signup")}
+      >
+        <Text>Signup</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 50,
+    color: "#34434D",
+    fontWeight: "bold",
+    marginBottom: 80,
+  },
+  subtitle: {
+    fontSize: 15,
+    color: "gray",
+    marginTop: 20,
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 30,
+    width: "80%",
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: "#FBFAF3",
+  },
+  button: {
+    backgroundColor: "#5D3FD3",
+    width: "30%",
+    height: 40,
+    padding: 10,
+    borderRadius: 30,
+    marginTop: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
+});
